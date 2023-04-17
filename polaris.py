861cff6f413dc542c6c59a722c175de454f73805
ab-dalton <73718652+ab-dalton@users.noreply.github.com>
2023-04-17 11:40:54 -0400

    Update polaris.py

--- polaris.py
+++ polaris.py
@@ -5,99 +5,56 @@ Created on Wed Apr 12 18:15:02 2023
 @author: adalt043
 """
 
-import rioxarray as rxr
-import numpy as np
-import pandas as pd
-import geopandas as gpd
 from geopandas import GeoDataFrame
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import seaborn as sns
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import shapely
-from shapely.geometry import Point
-import rasterio
-from rasterstats import zonal_stats
-import rasterstats as rs
-
-
-path = "D:/Abby/paper_3/polaris/drive-download-20230412T201907Z-001/POLARIS_RIO_EA_2012_TIF_v04/"
-
-polaris = rxr.open_rasterio("D:/Abby/paper_3/polaris/drive-download-20230412T201907Z-001/POLARIS_RIO_EA_2012_TIF_v04/ea_20120702_polaris_IA_RIO.tif")
-polaris_3347 = polaris.rio.reproject("EPSG:3347")
-polaris_3347.rio.to_raster("D:/Abby/paper_3/polaris/drive-download-20230412T201907Z-001/POLARIS_RIO_EA_2012_TIF_v04/ea_20120702_polaris_IA_RIO_3347.tif")
-
-polaris2 = rxr.open_rasterio("D:/Abby/paper_3/polaris/drive-download-20230412T201907Z-001/POLARIS_RIO_EA_2012_TIF_v04/ea_20120702_polaris_IA_RIO_3347.tif")
-
-polaris2.plot()
-
-df = dataarray[0].to_pandas()
+import geopandas as gpd
+import os
+import pandas as pd
 
 
 # -----------------------------------------------------------------------------
-# Create grid
-# Projection: NAD83 Statistics Canada Lambert (EPSG 3347)
+# Load all shapefile data and concat
 # -----------------------------------------------------------------------------
 
-# Set grid extents - utm mercator values to set corners in m
-# Baffin Bay
-xmin = 6000000
-ymin = 1900000
-xmax = 9500000
-ymax = 5000000
+# Set the directory path where the shapefiles are located
+folder_path = "D:/Abby/paper_3/polaris/50km_points/"
 
-# Cell size
-# Baffin Bay
-cell_size = 50000  # cell size in m needs to be divisible by extents above
+# Create an empty list to store the GeoDataFrames
+gdfs = []
 
-# Create the cells in a loop
-grid_cells = []
-for x0 in np.arange(xmin, xmax + cell_size, cell_size):
-    for y0 in np.arange(ymin, ymax + cell_size, cell_size):
-        # Bounds
-        x1 = x0 - cell_size
-        y1 = y0 + cell_size
-        grid_cells.append(shapely.geometry.box(x0, y0, x1, y1))
-        
-# Set grid projection
-grid = gpd.GeoDataFrame(grid_cells, columns=["geometry"], crs="epsg:3347")
+# Loop through each shapefile in the directory and read it into a GeoDataFrame
+for file in os.listdir(folder_path):
+    if file.endswith(".shp"):
+        filepath = os.path.join(folder_path, file)
+        gdf = gpd.read_file(filepath)
+        # Add a new column to the GeoDataFrame with the filename as the value
+        gdf['filename'] = file[:-4]
+        # Append the GeoDataFrame to the list
+        gdfs.append(gdf)
 
-# Create grid coordinates (cheap centroid)
-grid["coords"] = grid["geometry"].apply(lambda x: x.representative_point().coords[:])
-grid["coords"] = [coords[0] for coords in grid["coords"]]
+# Concatenate all the GeoDataFrames into a single GeoDataFrame
+merged_gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True), crs=gdf.crs)
 
+gdf = GeoDataFrame(merged_gdf, crs="epsg:3995")
+gdf_3347 = merged_gdf.to_crs(epsg=3347)
 
-# -----------------------------------------------------------------------------
-# Plot grid
-# -----------------------------------------------------------------------------
+# Clip sea ice database to NCAA zone 
+ncaa_poly = gpd.read_file("D:/Abby/paper_3/polaris/ncaa_poly.shp")
+ncaa_poly = ncaa_poly.to_crs(epsg=3347)
+merged_clip = gpd.clip(gdf_3347, ncaa_poly)
+merged_clip.plot()
 
-# Plot grid
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-grid.plot(
-    edgecolor="black",
-    color="white",
-    ax=ax,
-    legend=True,
+# Split filename into columns for date and ice class
+merged_clip['date'] = merged_clip['filename'].str.split('_').str[1]
+merged_clip['ice_class'] = merged_clip['filename'].str.split('_').str[3] + merged_clip['filename'].str.split('_').str[4]
+merged_clip['ice_class'] = merged_clip['ice_class'].str.replace('RIO', '')
+
+# Convert date column to datetime
+
+merged_clip['date'] = pd.to_datetime(
+    merged_clip['date'].astype(str), format="%Y%m%d"
 )
 
-# Observe distortion of grid with WGS84 (EPSG 4326) projection -- 
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-grid.to_crs(4326).plot(
-    edgecolor="black",
-    color="white",
-    ax=ax,
-    legend=True,
-)
 
-# -----------------------------------------------------------------------------
-# Calculate zonal stats of raster using grid
-# -----------------------------------------------------------------------------
-
-stats = rs.zonal_stats(grid, polaris2, stats=['mean', 'max', 'min', 'median'])
-
-
-
+merged_clip.to_csv("D:/Abby/paper_3/polaris/polaris_clipped.csv")
 
 
