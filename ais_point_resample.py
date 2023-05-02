e096850a03657eafe638e660d1500f27152dcbc2
ab-dalton <73718652+ab-dalton@users.noreply.github.com>
2023-05-02 13:45:00 -0400

    Update ais_point_resample.py

--- ais_point_resample.py
+++ ais_point_resample.py
@@ -5,7 +5,6 @@ Created on Tue May  2 12:58:58 2023
 @author: adalt043
 """
 
-
 import pandas as pd
 
 # -----------------------------------------------------------------------------
@@ -13,24 +12,23 @@ import pandas as pd
 # -----------------------------------------------------------------------------
 
 # Load CI2D3 data 
-ci2d3 = pd.read_csv("D:/Abby/paper_3/CI2D3/ci2d3_1.1b_Abby.csv", index_col=False)
+ais = pd.read_csv("D:/Abby/paper_3/AIS_tracks/SAIS_eE_2019_06_ManuvPts_Canada/2019_06_pts_nordreg.csv", index_col=False)
 
 # Convert to datetime
-ci2d3["scenedate"] = pd.to_datetime(ci2d3["scenedate"].astype(str), format="%Y-%m-%d %H:%M:%S")
+ais["YMD_HMS"] = pd.to_datetime(ais["YMD_HMS"].astype(str), format="%d/%m/%Y")
 
-# Define the 2-week intervals
-intervals = pd.date_range(start='2008-07-13', end='2013-12-31', freq='2W')
+# Define the daily intervals
+intervals = pd.date_range(start='2012-07-01', end='2019-12-31', freq='D')
 
 # Add a new column to the DataFrame with the interval labels
-ci2d3['interval'] = pd.cut(ci2d3['scenedate'], bins=intervals, labels=range(len(intervals)-1))
+ais['interval'] = pd.cut(ais['YMD_HMS'], bins=intervals, labels=range(len(intervals)-1))
 
-# Group by interval and branch then get the first row of each group
-ci2d3_unique = ci2d3.groupby(['interval', 'branch']).first().reset_index()
+# Group by interval and mmsi then get the first row of each group
+ais_daily_mmsi = ais.groupby(['interval', 'mmsi']).first().reset_index()
 
-# Drop all rows where there is no unique branch observation within the biweekly interval
-ci2d3_unique_branch = ci2d3_unique.dropna(subset=['imgref'])
+# Drop all rows where there is no unique branch observation within the daily interval
+ais_daily_mmsi = ais_daily_mmsi.dropna(subset=['YMD_HMS'])
 
-# Filter DataFrame based on shipping season (July to October)
-ci2d3_unique_branch_jaso = ci2d3_unique_branch[(ci2d3_unique_branch['scenedate'].dt.month >= 7) & (ci2d3_unique_branch['scenedate'].dt.month <= 10)]
+ais_daily_mmsi.to_csv('D:/Abby/paper_3/AIS_tracks/2019_06_daily_pts.csv')
 
-ci2d3_unique_branch_jaso.to_csv('D:/Abby/paper_3/CI2D3/ci2d3_1.1b_filtered.csv')
+
